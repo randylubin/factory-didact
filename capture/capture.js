85af5138ea7e9e759387f56921e478d3a00f38ee
Randy Lubin <randylubin@gmail.com>
2015-05-06 20:14:40 -0700

    image rotation

--- capture/capture.js
+++ capture/capture.js
@@ -122,11 +122,13 @@ angular.module('myApp.capture', ['firebase', 'ngRoute'])
 			})
 		}
 
+		scope.syncedData.dateLastEdited = new Date().toString();
 		scope.feedback.text = null
 			angular.forEach(scope.tags, function(tag){
 				tag.selected = false;
 		})
 		scope.smallPhoto = null;
+		scope.syncedData = location.search().observer;
 
 	}
 
