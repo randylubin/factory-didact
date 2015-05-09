3385f9ce08e2b270bf6a5c5b76e6158fb4e20401
Randy Lubin <randylubin@gmail.com>
2015-05-09 11:12:04 -0700

    Can now delete users

--- learnings/learnings.js
+++ learnings/learnings.js
@@ -12,20 +12,16 @@ angular.module('myApp.learnings', ['firebase', 'ngRoute'])
 .controller('LearningsCtrl', ['$scope', '$location', 'UserData', '$firebaseObject','$firebaseArray', '$document', function(scope, location, UserData, $firebaseObject, $firebaseArray, $document) {
 	scope.userData = UserData;
 	scope.learnings = null;
-	
+  scope.deleteModalHidden = true;
 
-	var ref = new Firebase("https://didact.firebaseio.com/users/");
-	//scope.userData.capturedFeedback = $firebaseArray(refActuals);
-
-	var syncObject = $firebaseObject(ref);
-	  // synchronize the object with a three-way data binding
-	  // click on `index.html` above to see it used in the DOM!
-	syncObject.$bindTo(scope, "learnings");
+  var refLearning = new Firebase("https://didact.firebaseio.com/users/");
+  scope.learnings = $firebaseArray(refLearning);
 
 	scope.goToUser = function(name) {
 		location.search('user', name)
 		location.path('/capture')
 	}
+
 
 
 }]);
